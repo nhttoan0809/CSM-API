7447074fa21b3a5d23e38231687517ea101fa46e
nhttoan0809 <111209609+nhttoan0809@users.noreply.github.com>
2022-12-02 19:07:04 +0700

    test redeploy in heroku

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,7 +4,7 @@ const connect = async () => {
     try {
         // await mongoose.connect('mongodb+srv://toanb1805926:toan123@sandbox.8exq5pz.mongodb.net/cold-warehouse')
         await mongoose.connect('mongodb+srv://toanb1805926:toan123@sandbox.8exq5pz.mongodb.net/cold-warehouse?retryWrites=true&w=majority')
-        console.log('Connect Successfully!!!')
+        console.log('Connect Successfully!!!!')
     } catch (error) {
         console.log('Connect failure!!!')
     }
